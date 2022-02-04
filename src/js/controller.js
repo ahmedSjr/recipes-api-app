9b4ca39448fddc39c240dca7ceef0450005dbf23
Ahmed Sirag <58979522+ahmedSjr@users.noreply.github.com>
2022-02-04 23:03:22 +0200

    Hash and load Event fucntions

--- src/js/controller.js
+++ src/js/controller.js
@@ -37,10 +37,14 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+
     renderSpinner(recipeContainer);
     //Loading recipe
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
@@ -159,4 +163,6 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+['hashchange', 'load'].forEach(event =>
+  window.addEventListener(event, showRecipe)
+);
