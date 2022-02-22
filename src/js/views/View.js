49ae1e594ae5b57affd1c1276e80b2fcc6705382
Ahmed Sirag <58979522+ahmedSjr@users.noreply.github.com>
2022-02-22 15:45:27 +0200

    Final build

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -3,6 +3,16 @@ import icons from 'url:../../img/icons.svg';
 export default class View {
   _data;
 
+  /**
+   * Render the received object to the dom
+   * @param {Object | Object[]} data The data to be render ex:recipe
+   * @param {boolean} [render = true] If false create a markup string
+   * @returns {undefined | string} A markup string is return if render = false
+   * @this {Object} View Instance
+   * @author Ahmed Sirag
+   * @throws Finish Implementations
+   */
+
   render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
